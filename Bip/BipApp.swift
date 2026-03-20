48727a28eb9682f65a035c1736d28ba85d0f4893
Joris <gitlab@brontojoris.com>
2026-03-20 16:26:45 +1100

    Initial Commit

---
+++ Bip/BipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipApp.swift
+//  Bip
+//
+//  Created by Joris de Beer on 20/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct BipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BipTests/BipTests.swift
@@ -0,0 +1,17 @@
+//
+//  BipTests.swift
+//  BipTests
+//
+//  Created by Joris de Beer on 20/3/2026.
+//
+
+import Testing
+@testable import Bip
+
+struct BipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
